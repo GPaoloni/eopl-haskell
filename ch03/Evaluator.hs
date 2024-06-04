8f666366d6c0878505682604b1b5d834ece0453e
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2024-06-04 12:35:54 -0300

    chore: IntExpr eval implemented (parsing broken)

--- ch03/Evaluator.hs
+++ ch03/Evaluator.hs
@@ -1,39 +1,55 @@
-module Evaluator () where
+module Evaluator (evalExpr) where
 
 import AST
+import Control.Monad
 import Env
 
 type LetEnv = Env String Int
 
 data Result = RInt Int | RBool Bool
+  deriving (Show)
 
 toInt :: Result -> Either String Int
 toInt (RInt n) = Right n
-toInt exp = Left ("Can't convert expression to Int: " ++ show exp)
+toInt r = Left ("Can't convert expression to Int: " ++ show r)
 
 toBool :: Result -> Either String Bool
 toBool (RBool b) = Right b
-toBool exp = Left ("Can't convert expression to Bool: " ++ show exp)
+toBool r = Left ("Can't convert expression to Bool: " ++ show r)
 
--- data Expr
---   = LetInt String IntExpr IntExpr
---   | LetBool String BoolExpr BoolExpr
---   | IntExpr IntExpr
---   | BoolExpr BoolExpr
---   deriving (Show)
 evalExpr :: LetEnv -> Expr -> Either String Result
+evalExpr env (IntExpr expr) = evalIntExpr env expr
 evalExpr _ _ = error "not implemented"
 
--- data Primary
---   = Int Int
---   | IntVar String
---   | Expr IntExpr
+evalIntExpr :: LetEnv -> IntExpr -> Either String Result
+evalIntExpr env (Term term) = evalTerm env term
+evalIntExpr _ _ = error "not implemented"
+
+-- data IntExpr
+--   = Term Term
+--   | IfIntExpr BoolExpr IntExpr IntExpr
 --   deriving (Show)
-evalPrimary :: LetEnv -> Primary -> Either String Result
-evalPrimary env (Int n) = Right (RInt n)
-evalPrimary env (IntVar var) =
-  maybe (Left $ "Unbound variable: " ++ var) (Right . RInt) (applyEnv env var)
-evalPrimary env (Expr exp) = evalExpr env exp
+
+evalTerm :: LetEnv -> Term -> Either String Result
+evalTerm env (Diff t f) =
+  case (evalTerm env t, evalFactor env f) of
+    (Right term, Right factor) ->
+      either (Left . show) Right $ do
+        f' <- toInt term
+        p' <- toInt factor
+        Right $ RInt (f' - p')
+    (Left e, _) -> Left e
+    (_, Left e) -> Left e
+evalTerm env (Plus t f) =
+  case (evalTerm env t, evalFactor env f) of
+    (Right term, Right factor) ->
+      either (Left . show) Right $ do
+        f' <- toInt term
+        p' <- toInt factor
+        Right $ RInt (f' + p')
+    (Left e, _) -> Left e
+    (_, Left e) -> Left e
+evalTerm env (Factor f) = evalFactor env f
 
 evalFactor :: LetEnv -> Factor -> Either String Result
 evalFactor env (Times f p) =
@@ -42,7 +58,26 @@ evalFactor env (Times f p) =
       either (Left . show) Right $ do
         f' <- toInt factor
         p' <- toInt primary
-        return (f' * p')
+        Right $ RInt (f' * p')
+    (Left e, _) -> Left e
+    (_, Left e) -> Left e
+evalFactor env (Div f p) =
+  case (evalFactor env f, evalPrimary env p) of
+    (Right factor, Right primary) ->
+      either (Left . show) Right $ do
+        f' <- toInt factor
+        p' <- toInt primary
+        when (p' == 0) (Left "types of cons and alt should match")
+        Right $ RInt (div f' p')
+    (Left e, _) -> Left e
+    (_, Left e) -> Left e
+evalFactor env (Primary p) = evalPrimary env p
+
+evalPrimary :: LetEnv -> Primary -> Either String Result
+evalPrimary env (Int n) = Right (RInt n)
+evalPrimary env (IntVar var) =
+  maybe (Left $ "Unbound variable: " ++ var) (Right . RInt) (applyEnv env var)
+evalPrimary env (Expr expr) = evalIntExpr env expr
 
 -- error ""
 
