9bbbf6fd408ec97e55d8b4e65b0ffa4ba3515abf
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2024-07-07 00:08:03 -0300

    chore: add extendEnvStar for multople updates

--- ch03/1-let/Env.hs
+++ ch03/1-let/Env.hs
@@ -1,17 +1,20 @@
-module Env (Env (..), emptyEnv, extendEnv, applyEnv, isEmpty) where
+module Env (Env, emptyEnv, extendEnv, extendEnvStar, applyEnv, isEmpty) where
 
 type Env a b = [(a, b)]
 
-emptyEnv :: Eq a => () -> Env a b
+emptyEnv :: (Eq a) => () -> Env a b
 emptyEnv () = []
 
-extendEnv :: Eq a => a -> b -> Env a b -> Env a b
+extendEnv :: (Eq a) => a -> b -> Env a b -> Env a b
 extendEnv k v env = (k, v) : env
 
-applyEnv :: Eq a => Env a b -> a -> Maybe b
-applyEnv [] var = Nothing
+extendEnvStar :: (Eq a) => [(a, b)] -> Env a b -> Env a b
+extendEnvStar xs env = foldr (:) env xs
+
+applyEnv :: (Eq a) => Env a b -> a -> Maybe b
+applyEnv [] _ = Nothing
 applyEnv ((k, v) : xs) var = if k == var then Just v else applyEnv xs var
 
-isEmpty :: Eq a => Env a b -> Bool
+isEmpty :: (Eq a) => Env a b -> Bool
 isEmpty [] = True
 isEmpty _ = False
